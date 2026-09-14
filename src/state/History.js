c1ef80abe1fbeec652e93bf7f085e138e6307f15
Mr-Dev-Loper <techworldhqofficial@gmail.com>
2026-09-14 19:26:16 +0500

    feat(state): implement History for undo/redo

--- src/state/History.js
+++ src/state/History.js
@@ -1,4 +1,42 @@
-// History management for undo/redo
 export default class History {
-    constructor() {}
+    constructor(maxSize = 50) {
+        this.undoStack = []
+        this.redoStack = []
+        this.maxSize = maxSize
+    }
+
+    push(previousState) {
+        this.undoStack.push(JSON.parse(JSON.stringify(previousState)))
+        if (this.undoStack.length > this.maxSize) {
+            this.undoStack.shift()
+        }
+        this.redoStack = []
+    }
+
+    undo(currentState) {
+        if (!this.canUndo()) return null
+        const state = this.undoStack.pop()
+        this.redoStack.push(JSON.parse(JSON.stringify(currentState)))
+        return state
+    }
+
+    redo(currentState) {
+        if (!this.canRedo()) return null
+        const state = this.redoStack.pop()
+        this.undoStack.push(JSON.parse(JSON.stringify(currentState)))
+        return state
+    }
+
+    canUndo() {
+        return this.undoStack.length > 0
+    }
+
+    canRedo() {
+        return this.redoStack.length > 0
+    }
+
+    clear() {
+        this.undoStack = []
+        this.redoStack = []
+    }
 }
